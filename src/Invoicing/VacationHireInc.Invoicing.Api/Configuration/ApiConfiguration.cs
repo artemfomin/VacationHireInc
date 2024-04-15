c250a2b9b3d0faf602eb4bf66b7820ce033b70ec
Artem Fomin <terra.integer@gmail.com>
2024-04-15 18:59:48 +0300

    Improve error handling in API endpoints
    Updated API endpoints in the configuration file to handle errors better when fetching invoices. If there are any errors in fetching the invoices, a "BadRequest" message is returned with the error messages. If no invoices are found, "NotFound" is returned. In addition, several classes have been made JSON serializable in the "AppJsonSerializerContext" file to handle different types of invoice request results.

--- src/Invoicing/VacationHireInc.Invoicing.Api/Configuration/ApiConfiguration.cs
+++ src/Invoicing/VacationHireInc.Invoicing.Api/Configuration/ApiConfiguration.cs
@@ -10,10 +10,23 @@ internal static class ApiConfiguration
         var invoiceApi = app.MapGroup("/invoices");
 
         invoiceApi.MapGet("/", async (HttpContext http) =>
-            await http.RequestServices.GetRequiredService<IInvoiceService>().GetInvoices(new CancellationToken()));
+        {
+            var invoices = await http.RequestServices.GetRequiredService<IInvoiceService>().GetInvoices(new CancellationToken());
+            if (invoices.Failed)
+                return Results.BadRequest(invoices.Errors.Select(x => x.Message));
+            
+            return invoices.Failed || !invoices.Payload.Any()
+                ? Results.NotFound()
+                : Results.Ok(invoices.Payload);
+        });
         
         invoiceApi.MapGet("/{id}", async (HttpContext http, Guid id) =>
-            await http.RequestServices.GetRequiredService<IInvoiceService>().GetInvoice(id, new CancellationToken()));
+        {
+            var invoice = await http.RequestServices.GetRequiredService<IInvoiceService>().GetInvoice(id, new CancellationToken());
+            return invoice is null
+                ? Results.NotFound()
+                : Results.Ok(invoice);
+        });
         
         invoiceApi.MapPost("/", async (HttpContext http, Invoice invoice) =>
             await http.RequestServices.GetRequiredService<IInvoiceService>().CreateInvoice(invoice, new CancellationToken()));
--- src/Invoicing/VacationHireInc.Invoicing.Api/Infrastructure/AppJsonSerializerContext.cs
+++ src/Invoicing/VacationHireInc.Invoicing.Api/Infrastructure/AppJsonSerializerContext.cs
@@ -1,10 +1,14 @@
 ﻿using System.Text.Json.Serialization;
+using VacationHireInc.Invoicing.Api.Core.Result;
 using VacationHireInc.Invoicing.Api.Models;
 
 namespace VacationHireInc.Invoicing.Api.Infrastructure;
 
 [JsonSerializable(typeof(Invoice[]))]
 [JsonSerializable(typeof(Invoice))]
+[JsonSerializable(typeof(Result<IList<Invoice>>))]
+[JsonSerializable(typeof(Result<List<Invoice>>))]
+[JsonSerializable(typeof(Result<Invoice>))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
 }
